8e48ea2746ae4f6378a8825f2bef12901e025082
Yan <podoyo23@gmail.com>
2025-01-12 11:37:32 +0800

    add:新增css 引入App.js 和 新增Day2 練習的渲染列表

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 // 項目的根組建
 // APP > 引入 index.js > public/index.html(root)
 
+import "./style.css"
+
 const count = 100
 function getName() {
 	return "John Doe"
 }
+const list = [
+	{ id: "1", name: "HTML" },
+	{ id: "2", name: "CSS" },
+	{ id: "3", name: "Vue" },
+	{ id: "4", name: "React" },
+	{ id: "5", name: "Angular" },
+	{ id: "6", name: "Node.js" },
+	{ id: "7", name: "MongoDB" },
+	{ id: "8", name: "Python" },
+]
 
 function App() {
 	return (
+		// Day1 練習
 		<div className="App">
-			this is App
-			{/*引號傳遞字串*/}
-			{"this is message"}
-			{/* 使用JS變量 */}
-			{count}
-			{/* 函數調用 */}
-			{getName()}
-			{/* 方法調用 */}
-			{new Date().getDate()}
-			{/* 使用JS對象 */}
-			<div style={{ color: "darkblue" }}>this is div</div>
+			<div>this is App</div>
+			<div>
+				{/*引號傳遞字串*/}
+				{"this is message"}
+			</div>
+			<div>
+				{/* 使用JS變量 */}
+				{count}
+			</div>
+			<div>
+				{/* 函數調用 */}
+				{getName()}
+			</div>
+			<div>
+				{/* 方法調用 */}
+				{new Date().getDate()}
+			</div>
+			<div>
+				{/* 使用JS對象 */}
+				<div style={{ color: "darkblue" }}>this is div</div>
+			</div>
+
+			<div>
+				{/* Day2練習  */}
+
+				{/* 渲染列表 */}
+				<ul className="custom-list">
+					{list.map((item) => (
+						<li key={item.id}>{item.name}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
